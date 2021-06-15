391e6e7ad6da61f7d46d93627dbb923cf3b110bd
wjddydgh94 <wjddydgh94@naver.com>
2021-06-15 22:40:14 +0900

    Feature: add social login

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return init ? <AppRouter isLoggedIn={isLoggedIn} /> : '불러오는중';
 }
 
 export default App;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { authService, firebaseInstance } from 'fbase';
 
-const AuthPage = () => <span>Auth</span>;
+const AuthPage = () => {
+  const onSocialClick = async (event) => {
+    const provider = new firebaseInstance.auth.GoogleAuthProvider();
+    const data = await authService.signInWithPopup(provider);
+  };
+
+  return (
+    <div>
+      <button onClick={onSocialClick}>구글 로그인 하기</button>
+    </div>
+  );
+};
 
 export default AuthPage;
