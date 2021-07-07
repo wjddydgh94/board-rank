1d9fdf6ddb556ac075dc85e65bce4b71d88fafe1
wjddydgh94 <wjddydgh94@naver.com>
2021-07-07 20:06:46 +0900

    Feat: 메인페이지 퍼블리싱 완료

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,6 @@ const Home = () => {
         ...document.data(),
         id: document.id,
       };
-      console.log(gameListObject);
       setGameList((prev) => [gameListObject, ...prev]);
     });
   };
@@ -22,7 +21,7 @@ const Home = () => {
   }, []);
 
   return (
-    <>
+    <section className="inner">
       <StyledGenryUl>
         <li className="active">전략</li>
         <li>추상</li>
@@ -32,18 +31,51 @@ const Home = () => {
         <li>파티</li>
         <li>테마</li>
       </StyledGenryUl>
-      {gameList.map((game) => (
-        <div key={game.id}>{game.detail}</div>
-      ))}
-    </>
+      <StyledGameUl>
+        {gameList.map((game) => (
+          <li key={game.id}>
+            <figure>
+              <img src={game.image} alt={`${game.name} 프로필 이미지`} />
+            </figure>
+            <div className="info-wrapper">
+              <div className="title-box">
+                <div className="title">
+                  <h3>{game.name}</h3>
+                  <p>
+                    {game.personnel}명 / {game.recommendation}명추천
+                  </p>
+                </div>
+                <div className="user-point">
+                  User point
+                  <span>
+                    {game.rating !== 0
+                      ? parseFloat(Math.round(game.rating) / 10).toFixed(1)
+                      : '평가 전'}
+                  </span>
+                </div>
+              </div>
+
+              <p className="detail">{game.detail}</p>
+
+              <p className="tag">
+                {game.genry.map((n) => {
+                  return '#' + n + ' ';
+                })}
+              </p>
+            </div>
+          </li>
+        ))}
+      </StyledGameUl>
+    </section>
   );
 };
 
 const StyledGenryUl = styled.ul`
   margin-top: 50px;
-  margin-bottom: 50px;
+  padding-bottom: 50px;
   display: flex;
   justify-content: center;
+  border-bottom: 1px solid #cfcfcf;
 
   li {
     margin: 0 25px;
@@ -62,6 +94,76 @@ const StyledGenryUl = styled.ul`
       font-weight: bold;
       color: #272727;
       border-bottom: 2px solid #000;
+    }
+  }
+`;
+
+const StyledGameUl = styled.ul`
+  li {
+    border-bottom: 1px solid #cfcfcf;
+    padding: 60px 93px;
+    display: flex;
+    align-items: center;
+
+    figure {
+      width: 172px;
+      margin-right: 30px;
+    }
+    .info-wrapper {
+      width: calc(100% - 295px);
+      .title-box {
+        border-bottom: 1px solid #000000;
+        padding: 0 10px 25px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 23px;
+        .title {
+          display: flex;
+          align-items: center;
+          h3 {
+            font-weight: 900;
+            font-size: 26px;
+            line-height: 100%;
+            color: #202020;
+            margin-right: 20px;
+          }
+          p {
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 100%;
+            color: #6a6a6a;
+          }
+        }
+        .user-point {
+          font-weight: normal;
+          font-size: 26px;
+          line-height: 100%;
+          color: #444444;
+          span {
+            font-weight: bold;
+            font-size: 26px;
+            line-height: 100%;
+            color: #444444;
+            margin-left: 12px;
+          }
+        }
+      }
+      .detail {
+        font-weight: 500;
+        font-size: 15px;
+        line-height: 160%;
+        color: #3e3e3e;
+        padding: 0 10px;
+        margin-bottom: 15px;
+      }
+      .tag {
+        padding: 0 10px;
+        font-weight: normal;
+        font-size: 15px;
+        line-height: 160%;
+        color: #505050;
+      }
     }
   }
 `;
