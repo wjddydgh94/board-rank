b6d7a957eeb0d3d354d065246727579884b1642c
wjddydgh94 <wjddydgh94@naver.com>
2021-07-22 14:15:06 +0900

    Feat: detail 페이지 게임 정보 호출

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import { dbService } from 'fbase';
 
 const Detail = () => {
   let { gameId } = useParams();
-  return <>{gameId}</>;
+
+  const [gameObj, setGameObj] = useState([]);
+
+  const getGameObj = async () => {
+    const dbGameObj = await dbService
+      .collection('game-list')
+      .where('id', '==', gameId)
+      .get();
+    dbGameObj.forEach((document) => {
+      const gameObjData = {
+        ...document.data(),
+      };
+      setGameObj(() => gameObjData);
+    });
+  };
+
+  useEffect(() => {
+    getGameObj();
+  }, []);
+
+  return <>{gameObj.name}</>;
 };
 
 export default Detail;
